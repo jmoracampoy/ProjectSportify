31f56b5c9ef59a4bc997d818a30a7b9e6fb54f49
LAPTOP-G787ICV4\jorge <jmc617@inlumine.ual.es>
2024-04-17 18:42:35 +0200

    test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json( { health: 'oOk' });
+  res.json( { health: 'ok' });
 });
 
 module.exports = router;
